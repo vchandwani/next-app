1cbc3137d09b796afdbbe9cd70f8a4df0b649dde
vchandwani <varunchandwani@gmail.com>
2026-09-09 15:26:40 +1000

    Changes for Caching for API
    Changes for Caching for API

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
+//Fetching in server component has extra benefits like caching, streaming, and avoiding CORS issues.Avoding fetching in client component is a good practice to avoid unnecessary re-renders and improve performance.
+// Next JS has caching built in for server components, so you can use the fetch API directly without any additional libraries. You can also use the revalidate option to specify how often the data should be re-fetched.
 
-const UsersPage = () => {
-  return (
-    <div>Users Page</div>
-  )
+interface User {
+    id: number;
+    name: string;   
+}
+
+const UsersPage = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users', {
+        next: { revalidate: 60 } // Revalidate the data every 60 seconds
+    });
+    const users: User[] = await res.json();
+
+    return (
+        <>
+            <h1>Users</h1>
+            <p>{new Date().toLocaleTimeString()}</p>
+            <ul>
+                {users.map((user: User) => (
+                    <li key={user.id}>{user.name}</li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default UsersPage
