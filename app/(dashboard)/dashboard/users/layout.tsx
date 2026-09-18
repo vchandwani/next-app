f6d0e9bf90a1a91dea13bdfcc5b7e90a107b2052
vchandwani <varunchandwani@gmail.com>
2026-09-18 11:05:03 +1000

    Changes for Dashboard
    Changes for Dashboard

--- app/(dashboard)/dashboard/users/layout.tsx
+++ app/(dashboard)/dashboard/users/layout.tsx
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Layout = ({children}: {children: React.ReactNode}) => {
-  return (
-    <div>
-        <>Users Insight</>
-        {children}
-    </div>
-  )
-}
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return <>{children}</>;
+};
 
-export default Layout
+export default Layout;
---
+++ app/components/DashboardHeader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardHeader = () => {
+  return (
+    <header className="border-b border-base-300">
+      <p className="p-2 center text-xl font-semibold">Dashboard</p>
+    </header>
+  );
+};
+
+export default DashboardHeader;
